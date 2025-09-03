a61e6ef3660dbcf3436e7a96ad5ea962e1a8f2d2
alekh-thakur <alekhrathor8@gmail.com>
2025-09-03 21:07:08 +0530

    Made some changes

---
+++ Aagman/script.js
@@ -0,0 +1,23 @@
+const loginForm = document.getElementById("loginForm");
+const errorMessage = document.getElementById("errorMessage");
+const loginButtonChange =  document.getElementById("login-signup");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (username === "admin" && password === "1234") {
+    localStorage.setItem("loggedIn", "true");
+    window.location.href = "../index.html";
+  } 
+  else {
+    alert("Please enter correct username & password");
+  }
+});
+
+function showError(msg) {
+  errorMessage.textContent = msg;
+  errorMessage.style.display = "block";
+}
